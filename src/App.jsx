3a699a4e62147e2557d45ce429a9cb847ae574dc
RZ <skywine13@gmail.com>
2025-01-06 23:41:14 +0800

    feat: reduce App component work.

--- src/App.jsx
+++ src/App.jsx
@@ -10,71 +10,12 @@ import NumLock from './NumLock';
 import useRcStore from './stores/rc.store.ts';
 
 function App () {
-  // const [ rcConfig, setRcConfig ] = useState({});
   const [ tab, setTab ] = useState('');
   const rcConfig = useRcStore(state => state.config);
   const saveRc = useRcStore(state => state.save);
 
   const switchTab = (event) => {
     setTab(event.currentTarget.value);
-  };
-
-  const save = () => {
-    // const newKeybind = Array
-    //   .from(document.querySelectorAll('.App .KeyBind tbody tr') || [])
-    //   .map(tr => {
-    //     const [ key, command ] = Array.from(tr.querySelectorAll('input[type=text]') || []);
-
-    //     return {
-    //       '@_key': key.placeholder,
-    //       action: { '@_name': 'Execute', '@_command': command.value }, // @_name can not be after @_command, or labwc can not understand.
-    //     };
-    //   });
-
-    // const commands = newKeybind.map(({ action }) => action['@_command']);
-    // const keys = newKeybind.map(oneBind => oneBind['@_key']);
-    const hints = [];
-    const { keybind } = rcConfig.keyboard;
-
-    if (keybind.some(keybind => keybind['@_key'].indexOf('-') < 1)) {
-      hints.push('at least one bind has no modifier key.');
-    }
-
-    if (keybind.map(keybind => keybind['@_key']).some((key, index, keys) => keys.indexOf(key) !== index)) {
-      hints.push('key duplicated between binds.');
-    }
-
-    if (keybind.some(({ action }) => action['@_command'] === '')) {
-      hints.push('at least one command not set yet.');
-    }
-
-    if (keybind
-      .map(({ action }) => action['@_command'])
-      .some((command, index, commands) => commands.indexOf(command) !== index)
-    ) {
-      hints.push('command duplicated between binds.');
-    }
-
-    if (hints.length > 0) {
-      return alert('Oops, something wrong, please check following found.\n- ' + hints.join('\n- '));
-    }
-
-    // if (rcConfig.keyboard) {
-    //   rcConfig.keyboard.keybind = newKeybind;
-    // } else {
-    //   rcConfig.keyboard = { keybind: newKeybind };
-    // }
-
-    // const builder = new XMLBuilder({ format: true, ignoreAttributes: false });
-
-    // const xmlContent = '<?xml version="1.0"?>\n' +
-    //   builder.build({ labwc_config: rcConfig }).replace(/><\/action>/g, ' />');
-
-    // writeTextFile('.config/labwc/rc.xml', xmlContent, { dir: BaseDirectory.Home })
-    //   .catch(error => console.error(error))
-    //   .then(error => { alert(error || 'config saved.'); });
-
-    saveRc(rcConfig);
   };
 
   return (
@@ -93,9 +34,6 @@ function App () {
           tab === 'autostart' && (<AutoStart />)
         }
       </main>
-      <footer>
-        <button onClick={save}>Save</button>
-      </footer>
     </div>
   );
 }
