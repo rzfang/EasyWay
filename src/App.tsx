519583778ca9c009463060c36f2e5626effb1648
RZ <skywine13@gmail.com>
2025-04-12 00:36:41 +0800

    feat: stop Launcher tab.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 
 import AutoStart from './AutoStart';
 import KeyBind from './KeyBind';
-import Launcher from './Launcher';
+// import Launcher from './Launcher';
 import NumLock from './NumLock';
 import TabBox from './TabBox';
 import Workspace from './Workspace';
@@ -18,7 +18,7 @@ function App (): ReactNode {
           { content: (<NumLock />), label: 'NumLock on/off' },
           { content: (<KeyBind />), label: 'Hotkeys' },
           { content: (<AutoStart />), label: 'Autostart' },
-          { content: (<Launcher />), label: 'Launchers' },
+          // { content: (<Launcher />), label: 'Launchers' },
           { content: (<Workspace />), label: 'Workspaces' },
           // { content: (<div></div>), label: 'About' },
         ]}
